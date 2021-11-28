1dcd466bae541b447ce2c855511032c451ce50d5
PedroHenrique-git <pedromf34@outlook.com>
2021-11-27 21:45:58 -0300

    add first files

---
+++ first-projet/src/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+	public static void main(String[] args) {
+		System.out.println("Teste");
+		int[] numbers = { 1, 2, 3, 4, 5 };
+
+		for (int i = 0; i <= numbers.length; i += 1) {
+			System.out.println(numbers[i]);
+		}
+	}
+}
